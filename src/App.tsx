a9b73fdfca7dfd382c60662768c744ac7babed7e
tanatuosi <49973816+tanatuosi@users.noreply.github.com>
2023-08-14 00:53:28 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import classNames from "classnames";
+import { useState } from "react";
+
 export function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onClick = () => {
+    const likeButton = document.querySelector(".like-module") as HTMLDivElement;
+    if (!likeButton) return;
+    if (likeButton.style.backgroundPosition !== "") {
+      likeButton.style.backgroundPosition = "";
+      setIsClicked(false);
+      return;
+    }
+    setIsClicked(true);
+    let currentPosition = 0;
+    const interval = setInterval(() => {
+      if (currentPosition > -2000) {
+        currentPosition -= 160;
+        likeButton.style.backgroundPosition = `${currentPosition}px 0`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 55);
+  };
+
   return (
-    <div className="h-full w-full flex items-center justify-center ">点赞</div>
+    <div className="h-full w-full flex items-center justify-center ">
+      <div
+        className={classNames(
+          "like-module",
+          isClicked ? "have-click" : "un-click"
+        )}
+        onClick={onClick}
+      ></div>
+    </div>
   );
 }
